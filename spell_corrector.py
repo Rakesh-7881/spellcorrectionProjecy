43a98a35fcc36cb045d00693bfbc7ec56a755db8
Rakesh-7881 <rakesh7881kadam@gmail.com>
2025-09-04 14:07:52 +0530

    Update spell_corrector.py
    updated

--- spell_corrector.py
+++ spell_corrector.py
@@ -5,5 +5,5 @@ def correct_spelling(text):
     return str(blob.correct())
 
 if __name__ == "__main__":
-    sentence = input("Enter a sentence: ")
+    sentence = input("Enter a sentence: Rakaesh ")
     print("Corrected:", correct_spelling(sentence))
